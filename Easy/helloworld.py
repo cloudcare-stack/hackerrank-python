c6f43a37722eddfb9b81004a3fd7b9c89a21743a
Colin McAteer <co8mcate@gmail.com>
2026-08-07 12:26:46 -0700

    first python script and some notes

---
+++ Easy/helloworld.py
@@ -0,0 +1,4 @@
+# This is a main function that prints "Hello, World!" to the console when executed.
+# I don't need to use main() function for this simple script, but it's a good practice to include it for larger programs.
+if __name__ == "__main__":
+    print("Hello, World!")
