af03fc13bbedebddf12c004a82e55c82a81b6343
Colin McAteer <co8mcate@gmail.com>
2026-08-09 20:16:58 -0700

    added scripts

---
+++ Easy/split_join.py
@@ -0,0 +1,10 @@
+'''
+Task: You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+'''
+def split_and_join(line):
+    return line.replace(" ", "-")
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
