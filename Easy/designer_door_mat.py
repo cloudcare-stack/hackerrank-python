298b1bdcfcb30031641e5f586a20765935c547a2
Colin McAteer <co8mcate@gmail.com>
2026-08-26 19:41:16 -0700

    Completed Designer Door Mat challenge

--- Easy/designer_door_mat.py
+++ Easy/designer_door_mat.py
@@ -1,3 +1,14 @@
 '''
+Designer Door Mat
 
 '''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n, m = map(int, input().split())
+
+for i in range(n // 2):
+    print(('.|.' * (2 * i + 1)).center(m, '-'))
+
+print('WELCOME'.center(m, '-'))
+
+for i in range(n // 2 - 1, -1, -1):
+    print(('.|.' * (2 * i + 1)).center(m, '-'))
